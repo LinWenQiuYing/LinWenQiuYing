2efd7dfb4ee76d7b650079bf2fb831ddbf09a91d
Lin Qiang <qiang.lin@transwarp.io>
2021-05-26 15:24:30 +0800

    初始化1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <main-footer></main-footer>
+  </div>
 </template>
 
 <script>
+import MainFooter from 'components/MainFooter'
 
 export default {
   name: 'App',
   components: {
-    
+    MainFooter,
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import url('assets/css/base.css');
 </style>
